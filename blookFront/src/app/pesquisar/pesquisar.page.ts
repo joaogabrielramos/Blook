54ee6f5ce97fd6e28cdcfc2c7bba0d405628a6f7
joaogabrielramos <bielzinhobk@hotmail.com>
2020-08-17 18:02:55 -0300

    joaoGabriel_#pesquisar

--- blookFront/src/app/pesquisar/pesquisar.page.ts
+++ blookFront/src/app/pesquisar/pesquisar.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import _ from 'lodash';
 
 @Component({
   selector: 'app-pesquisar',
@@ -7,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pesquisar.page.scss'],
 })
 export class PesquisarPage implements OnInit {
-  
+    users:Array<{name:string}>;
+    allUsers:any;
+    queryText:string;
 
 
-  constructor() { }
+  constructor() {
+    this.queryText ='';
+    this.users = [
+      {
+        name:'Gabriel'
+      },
+      {
+        name:'Douglas'
+      },
+      {
+        name:'Isabella'
+      },
+      {
+        name:'Ana'
+      }
+    ];
+
+    this.allUsers = this.users;
+
+   }
+
+   filterUser(us:any){
+     let val = us.target.value;
+     if(val && val.trim()!=''){
+       this.users = _.values(this.allUsers);
+       this.users = this.users.filter((user)=>{
+         return(user.name.toLowerCase().indexOf(val.toLowerCase())>-1);
+       })
+     }else{
+       this.users = this.allUsers;
+     }
+   }
 
   ngOnInit() {
 
