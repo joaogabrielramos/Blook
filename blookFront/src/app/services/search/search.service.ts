96d9bca3242f7722f8f81d040da32014fbcd42a0
joaogabrielramos <bielzinhobk@hotmail.com>
2020-08-18 02:31:12 -0300

    joaoGabriel_+integraçaoSearch_

---
+++ blookFront/src/app/services/search/search.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  
+  providedIn: 'root'
+})
+export class SearchService {
+  apiURL:string = 'http://localhost:8000/api/';
+
+  httpHeaders: any = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }
+
+
+  constructor(public http:HttpClient) { }
+
+
+  listUsers(): Observable<any>{
+    return this.http.get(this.apiURL +'listUsers')
+  }
+
+  listPosts():Observable<any>{
+    return this.http.get(this.apiURL+'listPosts')
+  }
+}
