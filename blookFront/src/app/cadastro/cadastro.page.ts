8670d72f739c34b5d53ae36320850667309e4f3d
joaogabrielramos <bielzinhobk@hotmail.com>
2020-08-19 23:43:05 -0300

    joaoGabriel_#tabmenuComponent_#login.scss_#register.scss_+comparacaoSenhaCadastro_

--- blookFront/src/app/cadastro/cadastro.page.ts
+++ blookFront/src/app/cadastro/cadastro.page.ts
@@ -1,6 +1,6 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 /* Toast */
 import { ToastController } from '@ionic/angular';
@@ -16,29 +16,38 @@ import { AuthService } from "./../services/auth.service";
 })
 export class CadastroPage implements OnInit {
 
+  //Criando o form
   registerForm: FormGroup;
 
+  
   constructor(public formbuilder:FormBuilder,
               public router: Router,
               public authService: AuthService,
               public toastController:ToastController
              ) {
+   
     this.registerForm = this.formbuilder.group({
       name:[null,[Validators.required]],
-      email:[null,[Validators.required, Validators.email]],
-      phone_number:[null,[Validators.required, Validators.maxLength(15)]],
-      password:[null, [Validators.required, Validators.minLength(8)]],
-      confirmPassword:[null, [Validators.required, Validators.minLength(8)]],
+      email:[null,[Validators.required,Validators.email]],
+      phone_number:[null,[Validators.required,Validators.maxLength(15)]],
+      password:[null,[Validators.required,Validators.minLength(8)]],
+      confirmpassword: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+      ])),
+      genre:[null,[Validators.required]],
       date_of_birth:[null,[Validators.required, Validators.maxLength(10)]],
-      genre: [null, [Validators.required]],
       image:[],
-    });
+    }, { 
+      validators: this.password.bind(this)
+     });
   }
 
   backToHome() {
     this.router.navigate(['/feed']);
   }
 
+  //Toast quando apertar o botão de cadastrar
   async presentToast() {
     const toast = await this.toastController.create({
       message: 'Cadastrado com sucesso. Seja bem vinda(o)!',
@@ -48,9 +57,10 @@ export class CadastroPage implements OnInit {
     toast.present();
   }
 
+  //Enviado o form pro console
   submitRegister(form) {
     console.log(form);
-
+    //Cadastrando
     this.authService.register(form.value).subscribe (
       (res) => {
         console.log(res);
@@ -68,4 +78,10 @@ export class CadastroPage implements OnInit {
   ngOnInit() {
   }
 
+// Conferere se as senhas coincidem
+  password(formGroup: FormGroup) {
+    const { value: password } = formGroup.get('password');
+    const { value: confirmPassword } = formGroup.get('confirmpassword');
+    return password === confirmPassword ? null : { passwordNotMatch: true };
+  }
 }
